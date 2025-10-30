dbcfd56fb4ca64bf30513f1f8cba758f22204c19
eliott.schott <eliott.schott@pixacare.com>
2025-10-30 15:50:57 +0100

    Update image name in build.yml and restore database configuration in config.js

--- api/config.js
+++ api/config.js
@@ -1,11 +1,10 @@
-module.exports =  {
-    ACCESS_TOKEN_SECRET : "EMMA123",
-    BDD : {
-    "host" :"dpg-d40798bipnbc73cf4njg-a.oregon-postgres.render.com",
-    "port" : "5432",
-    "user" : "cnam_qnjc_user",
-    "password" : "FOYjmLxOmqwdnfGPhMO4R8eZpErPzVXQ",
-    "bdname" :"cnam_qnjc" 
-    }
-}
-
+module.exports = {
+  ACCESS_TOKEN_SECRET: "EMMA123",
+  BDD: {
+    host: "dpg-d41mvajipnbc73fh9fm0-a.oregon-postgres.render.com",
+    port: "5432",
+    user: "pollution_db_user",
+    password: "C4ofdwhHhyCYjSDBHWDjN906zZ3PuDyw",
+    bdname: "pollution_db",
+  },
+};
